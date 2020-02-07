734582c3cdac583d88361cc1ffe603f0cb159572
FourmiSushi <neko@fourmisushi.soy>
2020-02-08 01:47:29 +0900

    org-modeを直した

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -5,54 +11,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("5e0b63e0373472b2e1cf1ebcc27058a683166ab544ef701a6e7f2a9f33a23726" "2a3ffb7775b2fe3643b179f2046493891b0d1153e57ec74bbe69580b951699ca" "e7666261f46e2f4f42fd1f9aa1875bdb81d17cc7a121533cad3e0d724f12faf2" "0d087b2853473609d9efd2e9fbeac088e89f36718c4a4c89c568dd1b628eae41" "1728dfd9560bff76a7dc6c3f61e9f4d3e6ef9d017a83a841c117bd9bebe18613" "f30aded97e67a487d30f38a1ac48eddb49fdb06ac01ebeaff39439997cbdd869" "3952ef318c8cbccf09954ecf43250ac0cbd1f4ae66b4abe569491b260f6e054b" default)))
- '(org-latex-classes
-   (quote
-    (("article" "\\documentclass[11pt,a4paper]{ltjsarticle}"
-      ("\\section{%s}" . "\\section*{%s}")
-      ("\\subsection{%s}" . "\\subsection*{%s}")
-      ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-      ("\\paragraph{%s}" . "\\paragraph*{%s}")
-      ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
-     ("report" "\\documentclass[11pt,a4paper]{ltjsreport}"
-      ("\\part{%s}" . "\\part*{%s}")
-      ("\\chapter{%s}" . "\\chapter*{%s}")
-      ("\\section{%s}" . "\\section*{%s}")
-      ("\\subsection{%s}" . "\\subsection*{%s}")
-      ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
-     ("book" "\\documentclass[11pt]{book}"
-      ("\\part{%s}" . "\\part*{%s}")
-      ("\\chapter{%s}" . "\\chapter*{%s}")
-      ("\\section{%s}" . "\\section*{%s}")
-      ("\\subsection{%s}" . "\\subsection*{%s}")
-      ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))))
- '(org-latex-compiler "lualatex")
- '(org-latex-default-packages-alist
-   (quote
-    (("AUTO" "inputenc" t
-      ("pdflatex"))
-     ("T1" "fontenc" t
-      ("pdflatex"))
-     ("" "graphicx" t nil)
-     ("" "longtable" nil nil)
-     ("" "wrapfig" nil nil)
-     ("" "rotating" nil nil)
-     ("normalem" "ulem" t nil)
-     ("" "amsmath" t nil)
-     ("" "textcomp" t nil)
-     ("" "amssymb" t nil)
-     ("" "capt-of" nil nil)
-     ("" "hyperref" nil nil))))
- '(org-latex-packages-alist
-   (quote
-    (("" "booktabs" nil)
-     ("" "siunitx" nil)
-     ("" "ascmac" nil)
-     ("" "mhchem" nil)
-     "\\sisetup{math-micro=\\text{µ},text-micro=µ}")))
+    ("6231254e74298a1cf8a5fee7ca64352943de4b495e615c449e9bb27e2ccae709" default)))
  '(package-selected-packages
    (quote
-    (go-mode yasnippet yasnipped slime-company ox-latex-subfigure org-plus-contrib lsp-ui lsp-haskell company-lsp exec-path-from-shell magit ido-vertical-mode amx ido-completing-read+ use-package smartparens slime lsp-mode doom-themes doom-modeline company-web))))
+    (magit ido-vertical-mode amx ido-completing-read+ slime-company go-mode lsp-ui lsp-haskell company-lsp lsp-mode yasnippet smartparens exec-path-from-shell doom-modeline org-plus-contrib use-package company))))
 
 ;; それぞれじゃまなのを消す
 (tool-bar-mode -1)
@@ -73,13 +35,12 @@
 (add-to-list 'default-frame-alist '(width . 96))
 
 ;; フォント
-(when (member "M+ 1mn" (font-family-list))
- (add-to-list 'default-frame-alist '(font . "Ubuntu Mono-12")))
-(create-fontset-from-ascii-font "M+ 1mn-16" nil "mplus1mn")
-(set-fontset-font "fontset-mplus1mn" 'unicode
-                 (font-spec :family "M+ 1mn" :size 16)
-                 nil 'prepend)
-(add-to-list 'default-frame-alist '(font . "fontset-mplus1mn"))
+(if (member "M+ 1mn" (font-family-list))
+    (progn (create-fontset-from-ascii-font "M+ 1mn-16" nil "mplus1mn")
+           (set-fontset-font "fontset-mplus1mn" 'unicode
+                             (font-spec :family "M+ 1mn" :size 16)
+                             nil 'prepend)
+           (add-to-list 'default-frame-alist '(font . "fontset-mplus1mn"))))
 
 ;; カーソル
 (add-to-list 'default-frame-alist '(cursor-type . bar))
@@ -110,18 +71,17 @@
 (setq-default indent-tabs-mode nil)
 ;; 選択範囲のハイライト
 (transient-mark-mode t)
-;; 行削除
+;; C-kで行削除
 (setq kill-whole-line 1)
-;; 最初のあれはいらん
+;; 起動時のもろもろを消す
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
-;; うるせぇ！！
+;; うるさいのを消す
 (setq ring-bell-function 'ignore)
-;; 邪魔
+;; メニューバーを消す
 (menu-bar-mode -1)
 
-
-;; パッケージ
+;; パッケージの設定
 (prog1 "setup package.el"
   (eval-when-compile (require 'package))
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -139,7 +99,7 @@
   (use-package doom-themes
     :ensure t
     :config
-    (progn (load-theme 'doom-sourcerer)
+    (progn (load-theme 'doom-laserwave)
            (doom-themes-org-config)))
   (use-package doom-modeline
     :ensure t
@@ -153,9 +113,9 @@
   :if (memq window-system '(mac ns x))
   :config (exec-path-from-shell-initialize))
 
-(use-package smartparens
-  :ensure t
-  :config (progn (use-package smartparens-config :config (smartparens-global-mode t))))
+(progn "smartparens" 
+       (use-package smartparens :ensure t)
+       (use-package smartparens-config :config (smartparens-global-mode t)))
 
 (use-package yasnippet :ensure t)
 
@@ -183,21 +143,66 @@
   (use-package lsp-ui :ensure t :commands lsp-ui)
   (use-package go-mode :ensure t))
 
-(prog1 "org"
-  (use-package ox-latex-subfigure :ensure t)
+(prog1 "org-mode"
   (use-package org
     :ensure t
-    :requires (org-plus-contrib ox-latex-subfigure)
-    :config (progn
-              (org-babel-do-load-languages
-               'org-babel-load-languages '((awk . t)
-                                           (shell . t)
-                                           (python . t)
-                                           (emacs-lisp . t)))
-              (use-package ox-latex :ensure t)
-              (use-package ox-bibtex :ensure t)
-              (setq org-latex-compiler "lualatex")
-              (setq org-latex-with-hyperref nil))))
+    :config
+    (org-babel-do-load-languages
+     'org-babel-load-languages '((awk . t)
+                                 (shell . t)
+                                 (python . t)
+                                 (emacs-lisp . t))))
+  (use-package org-plus-contrib
+    :ensure t
+    :no-require t
+    :after (org)
+    :config
+    (require 'ox-latex)
+    (require 'ox-bibtex)
+    :custom
+    ;; (org-latex-compiler "latexmk")
+    (org-latex-pdf-process '("latexmk"))
+    (org-latex-hyperref-template "\\hypersetup{\n pdfauthor={%a},\n pdftitle={%t}}\n")
+    (org-latex-classes
+     '(("article" "\\documentclass[11pt,a4paper]{ltjsarticle}"
+        ("\\section{%s}" . "\\section*{%s}")
+        ("\\subsection{%s}" . "\\subsection*{%s}")
+        ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+        ("\\paragraph{%s}" . "\\paragraph*{%s}")
+        ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+       ("report" "\\documentclass[11pt,a4paper]{ltjsreport}"
+        ("\\part{%s}" . "\\part*{%s}")
+        ("\\chapter{%s}" . "\\chapter*{%s}")
+        ("\\section{%s}" . "\\section*{%s}")
+        ("\\subsection{%s}" . "\\subsection*{%s}")
+        ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
+       ("book" "\\documentclass[11pt]{book}"
+        ("\\part{%s}" . "\\part*{%s}")
+        ("\\chapter{%s}" . "\\chapter*{%s}")
+        ("\\section{%s}" . "\\section*{%s}")
+        ("\\subsection{%s}" . "\\subsection*{%s}")
+        ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
+    (org-latex-default-packages-alist
+     '(("AUTO" "inputenc" t
+        ("pdflatex"))
+       ("T1" "fontenc" t
+        ("pdflatex"))
+       ("" "graphicx" t nil)
+       ("" "longtable" nil nil)
+       ("" "wrapfig" nil nil)
+       ("" "rotating" nil nil)
+       ("normalem" "ulem" t nil)
+       ("" "amsmath" t nil)
+       ("" "textcomp" t nil)
+       ("" "amssymb" t nil)
+       ("" "capt-of" nil nil)
+       ("" "hyperref" nil nil)))
+    (org-latex-packages-alist
+     '(("" "booktabs" nil)
+       ("" "siunitx" nil)
+       ("" "ascmac" nil)
+       ("" "mhchem" nil)
+       "\\sisetup{math-micro=\\text{µ},text-micro=µ}"))))
 
 (prog1 "slime"
   (use-package slime-company :ensure t)
@@ -228,11 +233,4 @@
             (setq ido-vertical-define-keys 'C-n-and-C-p-only)
             (setq ido-vertical-show-count t)))
 
-(use-package magit :ensure t :defer 2)
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package magit :ensure t)
