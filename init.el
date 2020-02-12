d25ed92df90ce1fc823e102bba8101993672b490
FourmiSushi <neko@fourmisushi.soy>
2020-02-12 19:50:09 +0900

    コメントの書き換え、Smartparens

--- init.el
+++ init.el
@@ -11,30 +11,28 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("6231254e74298a1cf8a5fee7ca64352943de4b495e615c449e9bb27e2ccae709" default)))
+    ("88a3c267ce2132defd46f2a4761925983dcbc35b1c3cfff1dded164ce169fed4" "6231254e74298a1cf8a5fee7ca64352943de4b495e615c449e9bb27e2ccae709" default)))
  '(package-selected-packages
    (quote
     (magit ido-vertical-mode amx ido-completing-read+ slime-company go-mode lsp-ui lsp-haskell company-lsp lsp-mode yasnippet smartparens exec-path-from-shell doom-modeline org-plus-contrib use-package company))))
 
-;; それぞれじゃまなのを消す
+;; ツールバー、スクロールバーとかを削除
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(menu-bar-mode -1)
 
-;; マウスホイールはあってもいい
+;; マウスホイールの設定
 (mouse-wheel-mode t)
 (setq mouse-wheel-scroll-amount '(2 ((shift) . 1))) ;; 2行ずつ動く
 (setq mouse-wheel-progressive-speed nil) ;; 加速しない
 (setq mouse-wheel-follow-mouse t) ;; いい感じにする
 
-;; クリップボード なんかデフォルトでこうなってたけど念の為設定ファイルでも
+;; クリップボードの設定
 (setq x-select-enable-clipboard t)
 (setq select-enable-clipboard t)
 (setq select-enable-primary nil)
 
-(add-to-list 'default-frame-alist '(height . 32))
-(add-to-list 'default-frame-alist '(width . 96))
-
-;; フォント
+;; フォントの設定
 (if (member "M+ 1mn" (font-family-list))
     (progn (create-fontset-from-ascii-font "M+ 1mn-16" nil "mplus1mn")
            (set-fontset-font "fontset-mplus1mn" 'unicode
@@ -42,22 +40,26 @@
                              nil 'prepend)
            (add-to-list 'default-frame-alist '(font . "fontset-mplus1mn"))))
 
-;; カーソル
+;; カーソルの見た目
 (add-to-list 'default-frame-alist '(cursor-type . bar))
 
-;; よくわからんファイルを作るな
+;; バックアップファイルの削除とか
 (setq make-backup-files nil)
 (setq delete-auto-save-files t)
-;; 括弧の対応きつくね？
+
+;; 対応する括弧をハイライトする
 (show-paren-mode t)
 (setq show-paren-delay 0)
 (setq show-paren-style 'parenthesis)
 
+;; コードの折りたたみを有効化
 (add-hook 'prog-mode-hook #'hs-minor-mode)
-;; 行番号
+
+;; 行番号を表示
 (setq display-line-numbers t)
 (global-display-line-numbers-mode)
-;; 言語周り(よくわからん)
+
+;; ロケール周り
 (set-locale-environment nil)
 (set-language-environment "Japanese")
 (set-terminal-coding-system 'utf-8)
@@ -66,20 +68,23 @@
 (setq default-buffer-file-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (prefer-coding-system 'utf-8)
-;; スペース2でインデントする時代
+
+;; インデント設定
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
+
 ;; 選択範囲のハイライト
 (transient-mark-mode t)
+
 ;; C-kで行削除
 (setq kill-whole-line 1)
-;; 起動時のもろもろを消す
+
+;; 起動時に表示されるものを消す
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
-;; うるさいのを消す
+
+;; 音を鳴らさない
 (setq ring-bell-function 'ignore)
-;; メニューバーを消す
-(menu-bar-mode -1)
 
 ;; パッケージの設定
 (prog1 "setup package.el"
@@ -89,13 +94,13 @@
   (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
   (package-initialize))
 
-(prog1 "install and load use-package"
+(prog1 "use-packageをインストールして読み込む"
   (unless (package-installed-p 'use-package)
     (package-refresh-contents)
     (package-install 'use-package))
   (eval-when-compile  (require 'use-package)))
 
-(prog1 "themes"
+(prog1 "カラーテーマ"
   (use-package doom-themes
     :ensure t
     :config
@@ -108,40 +113,34 @@
     :ensure t
     :config (message "M-X all-the-icons-install-fonts to install the fonts.")))
 
+;; PATHをbashから読み込む
 (use-package exec-path-from-shell
   :ensure t
   :if (memq window-system '(mac ns x))
   :config (exec-path-from-shell-initialize))
 
-(progn "smartparens" 
-       (use-package smartparens :ensure t)
-       (use-package smartparens-config :config (smartparens-global-mode t)))
+;; 括弧をいい感じに操作する
+(use-package smartparens
+         :ensure t
+         :config
+         (require 'smartparens-config)
+         (sp-use-smartparens-bindings)
+         :hook (prog-mode . company-mode))
 
+;; スニペット
 (use-package yasnippet :ensure t)
 
+;; 補完の表示
 (use-package company
   :ensure t
-  :config (progn
-            (setq company-idle-delay 0)
-            (setq company-minimum-prefix-length 1)
-            (setq company-selection-wrap-around t)
-            (delq 'company-files company-backends)
-            (add-to-list 'company-backends 'company-files)
-            (company-tng-configure-default))
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1)
+  (setq company-selection-wrap-around t)
+  (delq 'company-files company-backends)
+  (add-to-list 'company-backends 'company-files)
+  (company-tng-configure-default)
   :hook (prog-mode . company-mode))
-
-(prog1 "lsp"
-  (use-package lsp-mode
-    :ensure t
-    :commands lsp
-    :hook ((python-mode . lsp)
-           (haskell-mode . lsp)
-           (lsp-mode . lsp-ui-mode)
-           (go-mode . lsp)))
-  (use-package company-lsp :ensure t :commands company-lsp)
-  (use-package lsp-haskell :ensure t)
-  (use-package lsp-ui :ensure t :commands lsp-ui)
-  (use-package go-mode :ensure t))
 
 (prog1 "org-mode"
   (use-package org
@@ -209,28 +208,30 @@
   (use-package slime
     :ensure t
     :requires slime-compeny
-    :config (progn
-              (setq inferior-lisp-program "sbcl")
-              (setq slime-contribs '(slime-fancy slime-company)))))
+    :config
+    (setq inferior-lisp-program "sbcl")
+    (setq slime-contribs '(slime-fancy slime-company))))
 
-(ido-mode 1)
-(ido-everywhere 1)
-(setq ido-enable-flex-matching t)
-
-(use-package ido-completing-read+
+(prog1 "M-xの強化"
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (setq ido-enable-flex-matching t)
+  
+  (use-package ido-completing-read+
   :ensure t
   :config (ido-ubiquitous-mode 1))
-
-(use-package amx
+  
+  (use-package amx
+    :ensure t
+    :config
+    (amx-mode 1))
+  (use-package ido-vertical-mode
   :ensure t
-  :config (progn
-            (amx-mode 1)))
+  :config
+  (ido-vertical-mode 1)
+  (setq ido-vertical-define-keys 'C-n-and-C-p-only)
+  (setq ido-vertical-show-count t)))
 
-(use-package ido-vertical-mode
-  :ensure t
-  :config (progn
-            (ido-vertical-mode 1)
-            (setq ido-vertical-define-keys 'C-n-and-C-p-only)
-            (setq ido-vertical-show-count t)))
 
+;; Git対応
 (use-package magit :ensure t)
